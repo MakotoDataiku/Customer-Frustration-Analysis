94067cd0b1150407243f3889eb0ba02d181526e8
admin <admin>
2020-03-28 18:48:24 +0100

    Recipe ANALYSISOFCUSTOMERFRUSTRATIONSINAIRLINEINDUSTRIES.recipe from notebook TwitterScraper_1 saved through notebook notebook editor for recipe from notebook TwitterScraper_1

--- recipes/recipe from notebook TwitterScraper_1.py
+++ recipes/recipe from notebook TwitterScraper_1.py
@@ -10,6 +10,7 @@ from dataiku import pandasutils as pdu
 import pandas as pd
 from twitterscraper import query_tweets
 import datetime
+import ast
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 cols = ['screen_name', 'username', 'user_id', 'tweet_id', 'tweet_url', 'timestamp',
@@ -21,42 +22,45 @@ cols = ['screen_name', 'username', 'user_id', 'tweet_id', 'tweet_url', 'timestam
 df = pd.DataFrame(columns=cols)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-company = dataiku.get_custom_variables()['company']
-enddate = datetime.date.today()
-begindate = enddate - datetime.timedelta(5)
-list_of_tweets = query_tweets(company,
-                              begindate = begindate,
-                              enddate = enddate,
-                              poolsize = 10,
-                              lang = 'en')
+companies = dataiku.get_custom_variables()['company']
+companies_list = ast.literal_eval(companies)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-for row, tweet in enumerate(list_of_tweets):
-    for d in tweet.__dict__:
-        df.loc[row,'screen_name'] = tweet.screen_name
-        df.loc[row,'username'] = tweet.username
-        df.loc[row,'user_id'] = tweet.user_id
-        df.loc[row,'tweet_id'] = tweet.tweet_id
-        df.loc[row,'tweet_url'] = tweet.tweet_url
-        df.loc[row,'timestamp'] = tweet.timestamp
-        df.loc[row,'timestamp_epochs'] = tweet.timestamp_epochs
-        df.loc[row,'text'] = tweet.text
-        df.loc[row,'text_html'] = tweet.text_html
-        df.loc[row,'links'] = tweet.links
-        df.loc[row,'hashtags'] = tweet.hashtags
-        df.loc[row,'has_media'] = tweet.has_media
-        df.loc[row,'img_urls'] = tweet.img_urls
-        df.loc[row,'video_url'] = tweet.video_url
-        df.loc[row,'likes'] = tweet.likes
-        df.loc[row,'retweets'] = tweet.retweets
-        df.loc[row,'replies'] = tweet.replies
-        df.loc[row,'is_replied'] = tweet.is_replied
-        df.loc[row,'is_reply_to'] = tweet.is_reply_to
-        df.loc[row,'parent_tweet_id'] = tweet.parent_tweet_id
-        df.loc[row,'reply_to_users'] = tweet.reply_to_users
+for company in companies_list:
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-df["company"] = company
+    enddate = datetime.date.today()
+    begindate = enddate - datetime.timedelta(5)
+    list_of_tweets = query_tweets(company,
+                                  begindate = begindate,
+                                  enddate = enddate,
+                                  poolsize = 10,
+                                  lang = 'en')
+
+    for row, tweet in enumerate(list_of_tweets):
+        for d in tweet.__dict__:
+            df.loc[row,'screen_name'] = tweet.screen_name
+            df.loc[row,'username'] = tweet.username
+            df.loc[row,'user_id'] = tweet.user_id
+            df.loc[row,'tweet_id'] = tweet.tweet_id
+            df.loc[row,'tweet_url'] = tweet.tweet_url
+            df.loc[row,'timestamp'] = tweet.timestamp
+            df.loc[row,'timestamp_epochs'] = tweet.timestamp_epochs
+            df.loc[row,'text'] = tweet.text
+            df.loc[row,'text_html'] = tweet.text_html
+            df.loc[row,'links'] = tweet.links
+            df.loc[row,'hashtags'] = tweet.hashtags
+            df.loc[row,'has_media'] = tweet.has_media
+            df.loc[row,'img_urls'] = tweet.img_urls
+            df.loc[row,'video_url'] = tweet.video_url
+            df.loc[row,'likes'] = tweet.likes
+            df.loc[row,'retweets'] = tweet.retweets
+            df.loc[row,'replies'] = tweet.replies
+            df.loc[row,'is_replied'] = tweet.is_replied
+            df.loc[row,'is_reply_to'] = tweet.is_reply_to
+            df.loc[row,'parent_tweet_id'] = tweet.parent_tweet_id
+            df.loc[row,'reply_to_users'] = tweet.reply_to_users
+
+    df["company"] = company
 
 # Recipe outputs
 tweets = dataiku.Dataset("tweets")
