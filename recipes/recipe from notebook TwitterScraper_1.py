b867f9ab0f8d84ef745cec51ab39d40d1e3ce67b
admin <admin>
2020-03-28 18:51:52 +0100

    Recipe ANALYSISOFCUSTOMERFRUSTRATIONSINAIRLINEINDUSTRIES.recipe from notebook TwitterScraper_1 saved through notebook notebook editor for recipe from notebook TwitterScraper_1

--- recipes/recipe from notebook TwitterScraper_1.py
+++ recipes/recipe from notebook TwitterScraper_1.py
@@ -24,7 +24,7 @@ df = pd.DataFrame(columns=cols)
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 companies = dataiku.get_custom_variables()['company']
 companies_list = ast.literal_eval(companies)
-n_days = dataiku.get_custom_variables()['n_days']
+n_days = ast.literal_eval(dataiku.get_custom_variables()['n_days'])
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 for company in companies_list:
