40f593ba079009c6a475c0b363401c4d91fa3fcd
admin <admin>
2020-04-02 10:29:20 +0200

    edit files

---
+++ lib/python/aspect_clustering/get_aspect_freq_map/__init__.py
@@ -0,0 +1,6 @@
+def get_aspect_freq_map(aspects):
+    # simply counts how many times each noun appears
+    aspect_freq_map = defaultdict(int)
+    for asp in aspects:
+        aspect_freq_map[asp] += 1
+    return aspect_freq_map
