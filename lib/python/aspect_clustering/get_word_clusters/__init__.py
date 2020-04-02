f048d11ada7fa4ebe14ede7dd9dfb693232c8ce8
admin <admin>
2020-04-02 13:46:31 +0200

    edit files

--- lib/python/aspect_clustering/get_word_clusters/__init__.py
+++ lib/python/aspect_clustering/get_word_clusters/__init__.py
@@ -5,17 +5,16 @@ from sklearn import cluster
 
 
 def get_word_clusters(unique_aspects, nlp):
-    NUM_CLUSTERS = dataiku.get_custom_variables(typed=True)['NUM_CLUSTERS']
+    n_clusters = dataiku.get_custom_variables(typed=True)['NUM_CLUSTERS']
     print("Found {} unique aspects for this product".format(len(unique_aspects)))
     asp_vectors = get_word_vectors(unique_aspects, nlp) # gets the word vector for each noun 
     print("len(unique_aspects)", len(unique_aspects))
-    print("NUM_CLUSTERS", NUM_CLUSTERS)
-    if len(unique_aspects) < NUM_CLUSTERS:
+    print("n_clusters", n_clusters)
+    if len(unique_aspects) < n_clusters:
         print("Too few aspects ({}) found. No clustering required...".format(len(unique_aspects)))
         return list(range(len(unique_aspects)))
 
     print("Running k-means clustering...")
-    n_clusters = NUM_CLUSTERS
     kmeans = cluster.KMeans(n_clusters=n_clusters)
     kmeans.fit(asp_vectors)
     labels = kmeans.labels_
