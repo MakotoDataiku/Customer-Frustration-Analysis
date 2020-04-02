98f3f6b08f54eea1ba0860c588e456f002c888e3
admin <admin>
2020-04-02 11:19:15 +0200

    Recipe ANALYSISOFCUSTOMERFRUSTRATIONSINAIRLINEINDUSTRIES.compute_aspects_clustered saved through notebook notebook editor for compute_aspects_clustered

--- recipes/compute_aspects_clustered.py
+++ recipes/compute_aspects_clustered.py
@@ -6,15 +6,13 @@ from dataiku import pandasutils as pdu
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
-aspect_sentiment_pairs = dataiku.Folder("lgDPtGGq")
-aspect_sentiment_pairs_info = aspect_sentiment_pairs.get_info()
+aspect_sentiment_pairs = dataiku.Folder("aspect_sentiment_pairs")
+json_path = aspect_sentiment_pairs.file_path(filename='reviews_aspect_mapping.json')
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 import json
 import spacy
 from time import time
-import pandas as pd
-import numpy as np
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 from run_extraction.init_spacy import init_spacy
@@ -23,30 +21,31 @@ from run_extraction.init_spacy import init_spacy
 from aspect_clustering.update_reviews_data import update_reviews_data
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-def main():
-    time1 = time()
-    model_path='/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages/en_core_web_lg/en_core_web_lg-2.2.5'
-    nlp = init_spacy(model_path)
-    time2 = time()
-    print("----------------***----------------")
-    print("\nLoading aspect pairs file")
-    with open('data/processed/reviews_aspect_mapping.json', 'r') as fobj:
-        reviews_data = json.load(fobj)
-    print("Finished loading aspect pairs!!\n")
-    print("----------------***----------------")
-    time3 = time()
-    # update_reviews_data(reviews_data, nlp)
-    update_reviews_data(reviews_data, nlp)
-    # aspect_json_encoding.run("data/processed/model_results.json", "data/processed/model_results_encoding.json")
-    time4 = time()
-    print("Time for loading spacy: {0:.2}s".format(time2-time1))
-    print("Time for loading aspects json file: {0:.2}s".format(time3-time2))
-    print("Time for running aspect clustering: {0:.2}s".format(time4-time3))
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-main()
+time1 = time()
+model_path= dataiku.get_custom_variables()['model_path']
+nlp = init_spacy(model_path)
+time2 = time()
+print("----------------***----------------")
+print("\nLoading aspect pairs file")
+with open(json_path, 'r') as fobj:
+    reviews_data = json.load(fobj)
+print("Finished loading aspect pairs!!\n")
+print("----------------***----------------")
+time3 = time()
+# update_reviews_data(reviews_data, nlp)
+aspects_clustered_df, aspects_grouped_df = update_reviews_data(reviews_data, nlp)
+# aspect_json_encoding.run("data/processed/model_results.json", "data/processed/model_results_encoding.json")
+time4 = time()
+print("Time for loading spacy: {0:.2}s".format(time2-time1))
+print("Time for loading aspects json file: {0:.2}s".format(time3-time2))
+print("Time for running aspect clustering: {0:.2}s".format(time4-time3))
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 aspects_clustered = dataiku.Dataset("aspects_clustered")
 aspects_clustered.write_with_schema(aspects_clustered_df)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Write recipe outputs
+aspects_grouped = dataiku.Dataset("aspects_grouped")
+aspects_grouped.write_with_schema(aspects_grouped_df)
