fbc5e2b2e642d0710a9b49718e4513bdb9a2d108
admin <admin>
2020-04-02 10:43:30 +0200

    edit files

---
+++ lib/python/aspect_clustering/assign_groups/__init__.py
@@ -0,0 +1,32 @@
+from nltk.stem import WordNetLemmatizer
+from aspect_clustering.vector_dist import vector_dist
+
+def assign_groups(prod_id, df, nlp):
+    lemmatizer = WordNetLemmatizer() 
+    df['noun_lemmatized'] = df.noun.apply(lemmatizer.lemmatize)
+    df_grouped = df.groupby(["product_id", 'noun_lemmatized']).agg({'product_id':'size',
+                                                                    "polarity_nltk":'mean',
+                                                                    "polarity_textblob":'mean'}).rename(columns={'product_id':'count',
+                                                                                                   'polarity_nltk':'mean_polarity_nltk',
+                                                                                                   'polarity_textblob':'mean_polarity_textblob'}).reset_index()
+    print("There are %d nouns extracted" %(df_grouped.shape[0]))
+    
+    punctuality_vec = nlp('punctuality').vector
+    food_vec = nlp('food').vector
+    luggage_vec = nlp('luggage').vector
+    staff_vec = nlp('staff').vector
+    
+    asp_group = []
+    for aspect in df_grouped.noun_lemmatized:
+        dist_dic = {}
+        token_vector = nlp(aspect).vector
+        dist_dic['dist_punc'] = vector_dist(token_vector, punctuality_vec)
+        dist_dic['dist_food'] = vector_dist(token_vector, food_vec)
+        dist_dic['dist_lugg'] = vector_dist(token_vector, luggage_vec)
+        dist_dic['dist_staf'] = vector_dist(token_vector, staff_vec)
+        # group = min([dist_punc, dist_food, dist_lugg, dist_staf])
+        max_key = max(dist_dic, key=dist_dic.get)
+        asp_group.append(max_key)
+    df_grouped['group'] = asp_group
+    df_grouped.loc[df_grouped.noun_lemmatized==prod_id.lower(), "group"] = prod_id.lower()
+    return df_grouped
