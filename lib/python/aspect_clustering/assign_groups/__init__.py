89ce56e6231acca8491a61b7556788209510ab54
admin <admin>
2020-04-02 16:42:32 +0200

    edit files

--- lib/python/aspect_clustering/assign_groups/__init__.py
+++ lib/python/aspect_clustering/assign_groups/__init__.py
@@ -7,8 +7,8 @@ def assign_groups(prod_id, df, nlp):
     df_grouped = df.groupby(["product_id", 'noun_lemmatized']).agg({'product_id':'size',
                                                                     "polarity_nltk":'mean',
                                                                     "polarity_textblob":'mean'}).rename(columns={'product_id':'count',
-                                                                                                   'polarity_nltk':'mean_polarity_nltk',
-                                                                                                   'polarity_textblob':'mean_polarity_textblob'}).reset_index()
+                                                                                                                 'polarity_nltk':'mean_polarity_nltk',
+                                                                                                                 'polarity_textblob':'mean_polarity_textblob'}).reset_index()
     print("There are %d nouns extracted" %(df_grouped.shape[0]))
     
     punctuality_vec = nlp('punctuality').vector
