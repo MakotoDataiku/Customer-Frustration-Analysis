1319d358411ae9e63afaee305583ac88758da227
admin <admin>
2020-04-02 09:15:04 +0200

    edit files

---
+++ lib/python/run_extraction/__init__.py
@@ -0,0 +1,17 @@
+import spacy
+
+stopwords = dataiku.get_custom_variables()['stopwords']
+stopwords = ast.literal_eval(stopwords)
+
+exclude_stopwords = dataiku.get_custom_variables()['exclude_stopwords']
+exclude_stopwords = ast.literal_eval(exclude_stopwords)
+
+def init_spacy(model_path):
+    print("\nLoading spaCy Model....")
+    nlp = spacy.load(model_path)
+    print("spaCy successfully loaded")
+    for w in stopwords: # for the words in stopwords, set "is_stop == True" in spacy model
+        nlp.vocab[w].is_stop = True
+    for w in exclude_stopwords: # for the words in exclude_stopwords, set "is_stop == False" in spacy model
+        nlp.vocab[w].is_stop = False
+    return nlp
