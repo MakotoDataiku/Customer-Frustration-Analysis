67734553c47eb41981c3d4d15e5103f007bfe2b9
admin <admin>
2020-04-06 00:41:29 +0200

    edit files

--- lib/python/aspect_extraction/apply_extraction/__init__.py
+++ lib/python/aspect_extraction/apply_extraction/__init__.py
@@ -25,45 +25,8 @@ def apply_extraction(row, nlp, sid, text_column, review_id, product_id):
     doc=nlp(review_body)
     ner_heads = {ent.root.idx: ent for ent in doc.ents}
 
-    ## FIRST RULE OF DEPENDANCY PARSE -
-    ## M - Sentiment modifier || A - Aspect
-    ## RULE = M is child of A with a relationshio of amod
-    rule1_pairs = []
-    for token in doc:
-        A = "999999"
-        M = "999999"
-        if token.dep_ == "amod" and not token.is_stop:
-            M = token.text
-            if token.head.idx in ner_heads:
-                A = ner_heads[token.head.idx].text
-            else:
-                A = token.head.text
+    rule1_pairs = extract_rule1(doc, ner_heads)
 
-            # add adverbial modifier of adjective (e.g. 'most comfortable headphones')
-            M_children = token.children
-            for child_m in M_children:
-                if(child_m.dep_ == "advmod"):
-                    M_hash = child_m.text
-                    M = M_hash + " " + M
-                    break
-
-            # negation in adjective, the "no" keyword is a 'det' of the noun (e.g. no interesting characters)
-            A_children = token.head.children
-            for child_a in A_children:
-                if(child_a.dep_ == "det" and child_a.text == 'no'):
-                    neg_prefix = 'not'
-                    M = neg_prefix + " " + M
-                    break
-
-        if(A != "999999" and M != "999999"):
-            if A in prod_pronouns :
-                A = product_id
-            dict1 = {"noun" : A, 
-                     "adj" : M, 
-                     "rule" : 1, 
-                     "polarity_nltk" : sid.polarity_scores(M)['compound'],
-                     "polarity_textblob" : TextBlob(M).sentiment.polarity}
-            rule1_pairs.append(dict1)
 
     ## SECOND RULE OF DEPENDANCY PARSE -
     ## M - Sentiment modifier || A - Aspect
