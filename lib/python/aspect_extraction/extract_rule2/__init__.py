4aaf31f18a9ad2bd423a0f893c6fc4b65ef45ed1
admin <admin>
2020-04-06 00:42:18 +0200

    edit files

---
+++ lib/python/aspect_extraction/extract_rule2/__init__.py
@@ -0,0 +1,38 @@
+def extract_rule2(doc, ner_heads):
+    
+    rule2_pairs = []
+    for token in doc:
+        children = token.children
+        A = "999999"
+        M = "999999"
+        add_neg_pfx = False
+        for child in children :
+            if(child.dep_ == "nsubj" and not child.is_stop):
+                if child.idx in ner_heads:
+                    A = ner_heads[child.idx].text
+                else:
+                    A = child.text
+                # check_spelling(child.text)
+
+            if((child.dep_ == "dobj" and child.pos_ == "ADJ") and not child.is_stop):
+                M = child.text
+                #check_spelling(child.text)
+
+            if(child.dep_ == "neg"):
+                neg_prefix = child.text
+                add_neg_pfx = True
+
+    if (add_neg_pfx and M != "999999"):
+        M = neg_prefix + " " + M
+
+        if(A != "999999" and M != "999999"):
+            if A in prod_pronouns :
+                A = product_id
+            dict2 = {"noun" : A, 
+                     "adj" : M, 
+                     "rule" : 2, 
+                     "polarity_nltk" : sid.polarity_scores(M)['compound'],
+                     "polarity_textblob" : TextBlob(M).sentiment.polarity}
+            rule2_pairs.append(dict2)
+            
+    return rule2_pairs
