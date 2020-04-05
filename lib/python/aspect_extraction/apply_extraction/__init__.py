523594836d6ccebdabc8bc5d58fcdc014a12fa4f
admin <admin>
2020-04-06 00:46:31 +0200

    edit files

--- lib/python/aspect_extraction/apply_extraction/__init__.py
+++ lib/python/aspect_extraction/apply_extraction/__init__.py
@@ -20,73 +20,14 @@ def apply_extraction(row, nlp, sid, text_column, review_id, product_id):
     # star_rating = row['star_rating']
     # url = add_amazonlink(product_id)
 
-
-
     doc=nlp(review_body)
     ner_heads = {ent.root.idx: ent for ent in doc.ents}
 
     rule1_pairs = extract_rule1(doc, ner_heads)
     rule2_pairs = extract_rule2(doc, ner_heads)
-    
-
-
-    
-
-
-
     rule3_pairs = extract_rule3(doc, ner_heads)
-
-
-    ## FOURTH RULE OF DEPENDANCY PARSE -
-    ## M - Sentiment modifier || A - Aspect
-
-    #Adverbial modifier to a passive verb - A is a child of something with relationship of nsubjpass, while
-    # M is a child of the same something with relationship of advmod
-
-    #Assumption - A verb will have only one NSUBJ and DOBJ
-
-    rule4_pairs = []
-    for token in doc:
-
-
-        children = token.children
-        A = "999999"
-        M = "999999"
-        add_neg_pfx = False
-        for child in children :
-            if((child.dep_ == "nsubjpass" or child.dep_ == "nsubj") and not child.is_stop):
-                if child.idx in ner_heads:
-                    A = ner_heads[child.idx].text
-                else:
-                    A = child.text
-                # check_spelling(child.text)
-
-            if(child.dep_ == "advmod" and not child.is_stop):
-                M = child.text
-                M_children = child.children
-                for child_m in M_children:
-                    if(child_m.dep_ == "advmod"):
-                        M_hash = child_m.text
-                        M = M_hash + " " + child.text
-                        break
-                #check_spelling(child.text)
-
-            if(child.dep_ == "neg"):
-                neg_prefix = child.text
-                add_neg_pfx = True
-
-        if (add_neg_pfx and M != "999999"):
-            M = neg_prefix + " " + M
-
-        if(A != "999999" and M != "999999"):
-            if A in prod_pronouns :
-                A = product_id
-            dict4 = {"noun" : A, 
-                     "adj" : M, 
-                     "rule" : 4, 
-                     "polarity_nltk" : sid.polarity_scores(M)['compound'],
-                     "polarity_textblob" : TextBlob(M).sentiment.polarity}
-            rule4_pairs.append(dict4)
+    rule4_pairs = extract_rule4(doc, ner_heads)
+    
 
 
     ## FIFTH RULE OF DEPENDANCY PARSE -
---
+++ lib/python/aspect_extraction/extract_rule4/__init__.py
@@ -0,0 +1,49 @@
+def extract_rule4(doc, ner_heads):
+    ## FOURTH RULE OF DEPENDANCY PARSE -
+    ## M - Sentiment modifier || A - Aspect
+    #Adverbial modifier to a passive verb - A is a child of something with relationship of nsubjpass, while
+    # M is a child of the same something with relationship of advmod
+
+    #Assumption - A verb will have only one NSUBJ and DOBJ
+    
+    for token in doc:
+        children = token.children
+        A = "999999"
+        M = "999999"
+        add_neg_pfx = False
+        for child in children :
+            if((child.dep_ == "nsubjpass" or child.dep_ == "nsubj") and not child.is_stop):
+                if child.idx in ner_heads:
+                    A = ner_heads[child.idx].text
+                else:
+                    A = child.text
+                # check_spelling(child.text)
+
+            if(child.dep_ == "advmod" and not child.is_stop):
+                M = child.text
+                M_children = child.children
+                for child_m in M_children:
+                    if(child_m.dep_ == "advmod"):
+                        M_hash = child_m.text
+                        M = M_hash + " " + child.text
+                        break
+                #check_spelling(child.text)
+
+            if(child.dep_ == "neg"):
+                neg_prefix = child.text
+                add_neg_pfx = True
+
+        if (add_neg_pfx and M != "999999"):
+            M = neg_prefix + " " + M
+
+        if(A != "999999" and M != "999999"):
+            if A in prod_pronouns :
+                A = product_id
+            dict4 = {"noun" : A, 
+                     "adj" : M, 
+                     "rule" : 4, 
+                     "polarity_nltk" : sid.polarity_scores(M)['compound'],
+                     "polarity_textblob" : TextBlob(M).sentiment.polarity}
+            rule4_pairs.append(dict4)
+    
+    return rule4_pairs
