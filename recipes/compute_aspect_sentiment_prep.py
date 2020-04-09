bd73e5a4f585a3010b18cfe37de13c7b2de5e5c6
admin <admin>
2020-04-10 01:02:40 +0200

    Saved recipe: ANALYSISOFCUSTOMERFRUSTRATIONSINAIRLINEINDUSTRIES.compute_aspect_sentiment_prep

--- recipes/compute_aspect_sentiment_prep.py
+++ recipes/compute_aspect_sentiment_prep.py
@@ -60,6 +60,7 @@ df_grouped['group'] = asp_group
 df_grouped.loc[df_grouped.noun_lemmatized.str.lower() == df_grouped.product_id.str.lower(), "group"] = "company"
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# you have to cluster for each company
 n_clusters = dataiku.get_custom_variables(typed=True)['NUM_CLUSTERS']
 n_clusters = ast.literal_eval(n_clusters)
 kmeans = cluster.KMeans(n_clusters=n_clusters)
