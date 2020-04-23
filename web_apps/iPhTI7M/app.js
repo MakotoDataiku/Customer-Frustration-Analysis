51b0e7e404c0d11ef7400d275b270fe129e038f2
admin <makoto.miyazaki@dataiku.com>
2020-04-24 00:07:26 +0200

    Created web app Scrape tweets (ANALYSISOFCUSTOMERFRUSTRATIONSINAIRLINEINDUSTRIES.iPhTI7M)

---
+++ web_apps/iPhTI7M/app.js
@@ -0,0 +1,107 @@
+
+function init() {
+    var init_var = $.getJSON(getWebAppBackendUrl('init'), function(data){
+        console.log("received from backend : ", data);
+        d3.select("#init").text("");
+        var variables = data["data"];
+        
+        if (data["error"] != "") {
+            d3.select("#error").html(data["error"]);
+            d3.select("#init").html("ERROR");
+        }
+        else {
+        
+        Object.keys(variables).forEach(function(key,index) {
+        
+        box = d3.select("#variables").append("div")
+                                     .attr("class","form-group");
+            
+        box.append("label")
+           .attr("for",key)
+           .text(key);
+            
+        box.append("input")
+           .attr("type","text")
+           .attr("class","form-control")
+           .attr("id", key)
+           .attr("value", variables[key]);
+        });
+        
+        d3.select("#variables").append("button")
+                               .attr("class","btn btn-primary")
+                               .style("margin-top", "10px")
+                               .style("margin-right", "10px")
+                               .attr("id", "run_flow")
+                               .html('<i class="icon-play"></i> Run scenario : ' + data["scenario"])
+                               .attr("data-loading-text", "<i class='fa fa-circle-o-notch fa-spin'></i> Scenario running")
+                               .on("click", run_scenario);
+        
+        d3.select("#variables").append("a")
+                       .attr("id", "download_link")
+                       .attr("download", data["output"] + ".csv")
+                       .attr("href", getWebAppBackendUrl('download'));
+
+
+
+        d3.select("#download_link").append("button")
+                       .attr("class","btn btn-primary")
+                       .style("margin-top", "10px")
+                       .style("margin-right", "10px")
+                       .attr("id", "download")
+                       .html('<i class="icon-download-alt"></i> Download '+data["output"])
+                       .on("click", download);
+
+        }
+    })
+ }
+
+function run_scenario() {
+    
+    var $this = $(this);
+    $this.button('loading');
+
+    /* Getting all the input into an object that we will pass to the backend as a parameter of the call*/
+    variables = {};
+    $('.form-control').each(function(index){
+        variables[$(this).prop('id')] = $(this).val();
+    });
+    
+    d3.select("#download").attr("class","btn btn-primary disabled");
+    
+    console.log("sending to backend : ", variables);
+    
+    var flow_run =$.getJSON(getWebAppBackendUrl('run_scenario')+'/'+JSON.stringify(variables),function(data){
+        console.log(data);
+        
+        d3.select("#job_details").html(data["job_details"]);
+        
+        $this.button('reset');
+        d3.selectAll(".btn").attr("class", function(d) {
+            if (data["status"] == "SUCCESS") {return "btn btn-success"}
+            else { return "btn btn-danger" }});
+        
+         d3.select("#run_flow").text(function(d) {
+                                    if (data["status"] == "SUCCESS") {return "Scenario succeeded"}
+                                    else { return "Scenario Failed" }});
+        
+       
+
+        
+        $('#df_table').append(data['sample'])
+        $('#link_go_to').attr("href", data['link'])
+              .text("Go to " + data["output"]);
+        $('#df_table').show();
+        $('#job_details').show();
+
+        
+     }) 
+ }
+
+function download() {
+        var init_var = $.getJSON(getWebAppBackendUrl('download'),function(data){
+                console.log("received from backend : ", data)
+     })
+ } 
+
+
+init();
---
+++ web_apps/iPhTI7M/backend.py
@@ -0,0 +1,140 @@
+import dataiku
+import pandas as pd
+import json
+import dataiku
+import subprocess
+from flask import send_file
+from dataiku.scenario import Scenario
+
+# To Edit:
+OUTPUT_DATASET = "YOUR_DATASET"
+SCENARIO_ID = 'YOUR_SCENARIO'
+
+PROJECT_KEY = dataiku.get_custom_variables()["projectKey"]
+client = dataiku.api_client()
+p = client.get_project(PROJECT_KEY)
+
+
+@app.route('/init')
+def init():
+    data = p.get_variables()["standard"]
+    if len(data) == 0:
+        error = "<a id='link_var' target='_blank' href='../../variables/'> \
+                 <i class='icon-exclamation-sign'></i> \
+                 You need to add project variables.</a>"
+    else:
+        error = ""
+    if OUTPUT_DATASET not in [d["name"] for d in p.list_datasets()]:
+        error += "<br> \
+                     <i class='icon-exclamation-sign'></i> \
+                     Dataset {} does not exist -> \
+                     edit in python backend.".format(OUTPUT_DATASET)
+    if SCENARIO_ID not in [d["id"] for d in p.list_scenarios()]:
+        error += "<br> \
+                 <a id='link_var' target='_blank' href='../../scenarios/'> \
+                 <i class='icon-exclamation-sign'></i> \
+                 You need to create a scenario to compute {} \
+                 and edit the python backend. </a>".format(OUTPUT_DATASET)
+    return json.dumps({"status": "ok", "data": data, "scenario": SCENARIO_ID,
+                       "output": OUTPUT_DATASET, "error": error})
+
+
+@app.route('/run_scenario/<path:params>')
+def run_scenario(params):
+    params = json.loads(params)
+    # 1/ updates variables in project variables.
+    variables = p.get_variables()
+    variables["standard"] = params
+    p.set_variables(variables)
+    # 2/ Clean the dataset impacted by those variables.
+    for var_name in params.keys():
+        clear_var_impacted("${"+var_name+"}")
+    # 3/ Run the scenario.
+    scenar = p.get_scenario(SCENARIO_ID)
+    try:
+        scenar.run_and_wait()
+        jobSucceed = True
+    except:
+        jobSucceed = False
+    # 4/ Return the job status.
+    last_run = scenar.get_last_runs()[0]
+    details = last_run.get_details()
+    status = details['scenarioRun']['result']['outcome']
+    job_details = get_jobs_details(last_run)
+    if jobSucceed:
+        df = dataiku.Dataset(OUTPUT_DATASET).get_dataframe(limit=200)
+        sample = df.to_html(classes=['table'], border=0, index_names=0,
+                            max_rows=20, max_cols=8, na_rep="", index=False)
+        link = '../../datasets/'+OUTPUT_DATASET+'/explore/'
+        output = OUTPUT_DATASET
+    else:
+        sample = ""
+        jobId0 = last_run.get_details()["stepRuns"][0]
+        jobId = jobId0["additionalReportItems"][-1:][0]['jobId']
+        link = '../../jobs/'+jobId+"/"
+        output = "job error"
+    return json.dumps({"status": status, "output": output,
+                       "sample": sample, "link": link,
+                       "job_details": job_details})
+
+
+@app.route("/download")
+def download():
+    df = dataiku.Dataset(OUTPUT_DATASET).get_dataframe()
+    return df.to_csv(index=False)
+
+
+def get_jobs_details(l):
+    j = l.get_details()["stepRuns"][0]["additionalReportItems"]
+    job_list = [i["target"] for i in j]
+    d_list = [i["datasetName"] for i in job_list if "datasetName" in i]
+    job_list2 = [i["outcome"] for i in j]
+    run_list = pd.DataFrame(zip(d_list, job_list2))
+    run_list.columns = ["datasetName", "status"]
+    mapper = {"SUCCESS": '<i class="icon-thumbs-up-alt"></i>',
+              "WARNING": '<i class="icon-warning-sign"></i>',
+              "FAILED": '<i class="icon-thumbs-down"></i>'}
+    run_list["status"] = run_list.status.map(mapper)
+    out = run_list.to_html(classes='table', border=0,
+                           index=False, escape=0, header=0)
+    out = out.replace("\n", "")
+    return out
+
+
+def clear_var_impacted(var_name):
+    """ Clear the datasets where the recipe building it contains a ${variable}
+    """
+    variables = dataiku.get_custom_variables()
+    dip = variables['dip.home']
+    pK = variables["projectKey"]
+    dip += "/config/projects/"+pK+"/recipes/"
+
+    outputs = []
+    p = subprocess.Popen(
+                        "grep -l '" + var_name + "' " + dip + "/*",
+                        shell=True,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT
+                        )
+    for line in p.stdout.readlines():
+        if not line.endswith("json\n"):
+            print line
+            with open(
+                    dip + "/" + line.split("/")[-1:][0].split(".")[0]+".json"
+                    ) as data_file:
+                data = json.load(data_file)
+                try:
+                    for o in data["outputs"]["main"]["items"]:
+                        outputs.append(o["ref"])
+                except KeyError:
+                    print "ignore:", line
+
+    retval = p.wait()
+
+    # Clear outputs where the variable is used.
+    client = dataiku.api_client()
+    p = client.get_project(pK)
+    for o in list(set(outputs)):
+        d = p.get_dataset(o)
+        d.clear()
+        print o, " cleared"
