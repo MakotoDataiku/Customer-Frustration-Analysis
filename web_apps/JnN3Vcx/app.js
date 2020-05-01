c0f84fcaf187a52d2d96173c7f79a42ef0e64090
admin <makoto.miyazaki@dataiku.com>
2020-05-01 12:19:36 +0200

    Saved web app Airline Analysis (ANALYSISOFCUSTOMERFRUSTRATIONSINAIRLINEINDUSTRIES.JnN3Vcx)

--- web_apps/JnN3Vcx/app.js
+++ web_apps/JnN3Vcx/app.js
@@ -1,5 +1,21 @@
-$.getJSON(getWebAppBackendUrl('/first_api_call'), function(data) {
-    console.log('Received data from backend', data)
-    const output = $('<pre />').text('Backend reply: ' + JSON.stringify(data));
-    $('body').append(output)
-});
+setFilterOptions();
+
+function setFilterOptions(){
+    /* function to get distinct values of store id and product category and insert them in the dropdowns*/
+    let headers = new Headers()
+    let init = {
+        method : 'GET',
+        headers : headers
+    }
+
+    let url = getWebAppBackendUrl('/get_filter_values')
+    let promise = fetch(url, init) 
+                   .then(function(response){ 
+                       response.json()
+                               .then(function(data){
+                                let companiesList = data.companies;
+                                addOptions('companies', companiesList);                                
+                                })
+                   });
+                       
+}
