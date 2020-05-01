a538b46131617911f4809c4b16b905d85030f9e3
admin <makoto.miyazaki@dataiku.com>
2020-05-01 21:36:33 +0200

    Saved web app Airline Analysis (ANALYSISOFCUSTOMERFRUSTRATIONSINAIRLINEINDUSTRIES.JnN3Vcx)

--- web_apps/JnN3Vcx/app.js
+++ web_apps/JnN3Vcx/app.js
@@ -32,6 +32,7 @@ document.getElementById('button').onclick = function(){
 function getSelectedOption(id){
 /* function to get the selected value from the dropdowns */
     let select = document.getElementById(id);
+    console.log("select is", select)
     let value = select.options[select.selectedIndex].value;
     console.log("selected comapnies are", value)
     return value
