74340a62b3e4f261b24c756b4717e154c50eebe5
admin <makoto.miyazaki@dataiku.com>
2020-05-02 10:35:31 +0200

    Saved web app Airline Analysis (ANALYSISOFCUSTOMERFRUSTRATIONSINAIRLINEINDUSTRIES.JnN3Vcx)

--- web_apps/JnN3Vcx/app.js
+++ web_apps/JnN3Vcx/app.js
@@ -1,7 +1,9 @@
 let default_colors = ['#FC427B','#16a085','#3498db','#9b59b6','#7ed6df','#3B3EAC','#0099C6','#DD4477','#66AA00','#B82E2E','#316395','#994499','#22AA99','#AAAA11','#6633CC','#E67300','#8B0707','#329262','#5574A6','#3B3EAC']
 setFilterOptions();
 
-
+console.log($(document).ready(function() { 
+ $("#companies").select2();
+}))
 
 /* returns the company value chosen from the dropdowns */
 document.getElementById('button').onclick = function(){
@@ -29,12 +31,12 @@ document.getElementById('button').onclick = function(){
 
 function getSelectedOption(id){
 /* function to get the selected value from the dropdowns */
-    // let select = document.getElementById(id);
-    // console.log("select is", select)
-    // let value = select.options[select.selectedIndex].value;
-    // console.log("selected comapnies are", value)
-    // return value
-    return $('#' + id).val();
+    let select = document.getElementById(id);
+    console.log("select is", select)
+    let value = select.options[select.selectedIndex].value;
+    console.log("selected comapnies are", value)
+    return value
+    // return $('#' + id).val();
 }
 
 
