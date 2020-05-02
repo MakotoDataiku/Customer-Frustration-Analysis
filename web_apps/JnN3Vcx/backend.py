93c0af08828c51207b356ebd36d3c73479775a3f
admin <makoto.miyazaki@dataiku.com>
2020-05-02 10:53:04 +0200

    Saved web app Airline Analysis (ANALYSISOFCUSTOMERFRUSTRATIONSINAIRLINEINDUSTRIES.JnN3Vcx)

--- web_apps/JnN3Vcx/backend.py
+++ web_apps/JnN3Vcx/backend.py
@@ -14,7 +14,7 @@ table_name = "Cat_analysis_by_companies"
 def get_dataset_selection(company):
     df = dataiku.Dataset(table_name).get_dataframe()
     # df = df[df.product_id == company][['group', 'weighted_ave_tb']]
-    df = df["product_id"].isin([company])[['group', 'weighted_ave_tb']]
+    df = df["product_id"].isin(company)[['group', 'weighted_ave_tb']]
     print("df.columns", df.columns)
     print("df.product_id.unique()", df.product_id.unique())
     return df
