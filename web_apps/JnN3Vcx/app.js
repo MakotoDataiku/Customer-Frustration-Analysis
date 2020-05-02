425ecc3212b8383488b7b60263253c7392815089
admin <makoto.miyazaki@dataiku.com>
2020-05-02 21:23:22 +0200

    Saved web app Airline Analysis (ANALYSISOFCUSTOMERFRUSTRATIONSINAIRLINEINDUSTRIES.JnN3Vcx)

--- web_apps/JnN3Vcx/app.js
+++ web_apps/JnN3Vcx/app.js
@@ -113,7 +113,7 @@ function addGroupBarChart(data, labels) {
         dataset_company = {
             label:company,
             // hoverBackgroundColor:default_colors[i],
-            backgroundColor: 'rgba(0, 0, 0, 0.9)',
+            backgroundColor: default_colors[i],
             data:values};
         console.log("dataset_company", dataset_company);
         arr.push(dataset_company);
