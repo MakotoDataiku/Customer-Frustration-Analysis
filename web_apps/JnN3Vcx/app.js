20ea04dd497fbe7ca6b6dafba3e7ab7911bc91e9
admin <makoto.miyazaki@dataiku.com>
2020-05-02 12:23:35 +0200

    Saved web app Airline Analysis (ANALYSISOFCUSTOMERFRUSTRATIONSINAIRLINEINDUSTRIES.JnN3Vcx)

--- web_apps/JnN3Vcx/app.js
+++ web_apps/JnN3Vcx/app.js
@@ -97,3 +97,42 @@ function addBarChart(data, labels) {
         }
     });
 }
+
+
+let ctx = document.getElementById("table_stats").getContext("2d");
+
+let data = {
+    labels: ["company", "food", "luggage", "punctuality", "staff"],
+    datasets: [
+        {
+            label: "Blue",
+            backgroundColor: "blue",
+            data: [3,7,4]
+        },
+        {
+            label: "Red",
+            backgroundColor: "red",
+            data: [4,3,5]
+        },
+        {
+            label: "Green",
+            backgroundColor: "green",
+            data: [7,2,6]
+        }
+    ]
+};
+
+var myBarChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: {
+        barValueSpacing: 20,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    min: 0,
+                }
+            }]
+        }
+    }
+});
