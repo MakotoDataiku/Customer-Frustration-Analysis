6bd5cb4f6109f4cad143a84df84275ac91ba56a4
admin <makoto.miyazaki@dataiku.com>
2020-05-06 10:57:16 +0200

    Saved web app Airline Analysis (ANALYSISOFCUSTOMERFRUSTRATIONSINAIRLINEINDUSTRIES.JnN3Vcx)

--- web_apps/JnN3Vcx/app.js
+++ web_apps/JnN3Vcx/app.js
@@ -27,9 +27,9 @@ function onBarClicked(company, category) {
                    .then(function(response) {
                        response.json()
                        .then(function(data){
-                           console.log("data.table.table_pos", data.table.table_pos)
-                           document.getElementById('table_stats_pos').innerHTML = data.table.table_pos;
-                           document.getElementById('table_stats_neg').innerHTML = data.table.table_neg;
+                           //console.log("data.table.table_pos", data.table.table_pos)
+                           document.getElementById('table_stats_pos').innerHTML = data.table_pos;
+                           document.getElementById('table_stats_neg').innerHTML = data.table_neg;
                        })
                    });
 };
