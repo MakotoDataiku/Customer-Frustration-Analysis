70c08db8ee2647db7b91039f4d2e2c4df38b8307
admin <makoto.miyazaki@dataiku.com>
2020-05-09 13:13:37 +0200

    Recipe ANALYSISOFCUSTOMERFRUSTRATIONSINAIRLINEINDUSTRIES.compute_URL_removed saved through notebook notebook editor for compute_URL_removed

--- recipes/compute_URL_removed.py
+++ recipes/compute_URL_removed.py
@@ -1,18 +1,32 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+import re
+import unicodedata
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 tweepy_stacked = dataiku.Dataset("tweepy_stacked")
-tweepy_stacked_df = tweepy_stacked.get_dataframe()
+df = tweepy_stacked.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df['text'] = df.text.str.replace(u'\xa0', u' ')
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+def remove_URL(text):
+    return re.sub(r"""((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|(([^\s()<>]+|(([^\s()<>]+)))*))+(?:(([^\s()<>]+|(([^\s()<>]+)))*)|[^\s`!()[]{};:'".,<>?«»“”‘’]))""", "", text)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df['text']=df.text.apply(remove_URL)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
-url_removed_df = tweepy_stacked_df # For this sample code, simply copy input to output
+url_removed_df = df # For this sample code, simply copy input to output
 
 
 # Write recipe outputs
