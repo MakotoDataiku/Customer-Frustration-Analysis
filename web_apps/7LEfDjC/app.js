c1281c40e59c4a5c3d7b91c8bc3dfbf28d453a46
admin <makoto.miyazaki@dataiku.com>
2020-05-18 00:07:03 +0200

    Copied web app Airline Analysis 0.2 into Airline Analysis 0.3 (ANALYSISOFCUSTOMERFRUSTRATIONSINAIRLINEINDUSTRIES.gmnYUpH into ANALYSISOFCUSTOMERFRUSTRATIONSINAIRLINEINDUSTRIES.7LEfDjC)

---
+++ web_apps/7LEfDjC/app.js
@@ -0,0 +1,281 @@
+let default_colors = ['#FC427B','#16a085','#3498db','#9b59b6',
+                      '#7ed6df','#3B3EAC','#0099C6','#DD4477',
+                      '#66AA00','#B82E2E','#316395','#994499',
+                      '#22AA99','#AAAA11','#6633CC','#E67300',
+                      '#8B0707','#329262','#5574A6','#3B3EAC'];
+setFilterOptions();
+
+console.log($(document).ready(function() {
+ $("#companies").select2();
+}))
+
+function onBarClicked(company, category) {
+    let headers = new Headers();
+    let init = {
+        method : 'GET',
+        headers : headers
+    };
+    let params = {
+        'company':company,
+        'category':category
+    };
+    console.log("stringify json", JSON.stringify(params))
+    
+    let url = getWebAppBackendUrl('/get_table')+'/'+JSON.stringify(params);
+    let promise = fetch(url, init) 
+                   .then(function(response) {
+                       response.json()
+                       .then(function(data){
+                           let title_pos = 'Top 5 positive sentiment contributors for ' + company + " in " + category;
+                           let title_neg = 'Top 5 negative sentiment contributors for ' + company + " in " + category;
+                           document.getElementById('title_pos').innerHTML = title_pos;
+                           document.getElementById('title_neg').innerHTML = title_neg;
+                           addRows("insert_table_pos", data["table_pos"]);
+                           addRows("insert_table_neg", data["table_neg"]);
+                           addRowHandlers("insert_table_pos", data["table_pos"], company, category);
+                           addRowHandlers("insert_table_neg", data["table_neg"], company, category);
+                       })
+                   });
+};
+
+
+function addRowHandlers(id, data, company, category) {
+    
+    var table = document.getElementById(id);
+    var rows = table.getElementsByTagName("tr");
+    for (i = 0; i < rows.length; i++) {
+        row = table.rows[i];
+        row.onclick = function(){
+            let cell = this.getElementsByTagName("td")[0];
+            let topic = cell.innerHTML;
+            console.log("clicked topic", topic);
+            
+            let dataJson=JSON.parse(data)
+            console.log("Object.keys(dataJson)", Object.keys(dataJson))
+            const found_key = Object.keys(dataJson).find(key => dataJson[key]['topics'] === topic);
+            console.log("found_key", found_key)
+            let review_id = dataJson[found_key]['review_id']
+            // console.log("review_id", review_id)
+            
+            let headers = new Headers();
+            let init = {
+                method : 'GET',
+                headers : headers
+            };
+            let params = {
+                'company':company,
+                'category':category,
+                'review_id':review_id,
+                'topic':topic
+            };
+            let url = getWebAppBackendUrl('/get_tweets_table')+'/'+JSON.stringify(params);
+            let promise = fetch(url, init).then(function(response) {
+                response.json()
+                    .then(function(data){
+                    console.log("this is how tweets look like", data['0'])
+                    // document.getElementById('tweet_table').innerHTML = data;
+                    let title = 'Tweets about "' + topic + '" for ' + company;
+                    document.getElementById('title_tweets').innerHTML = title;
+                    addTweetRows('insert_tweet_table', data)
+                    
+                });
+            });
+        };
+    };
+};
+
+/* returns the company value chosen from the dropdowns */
+document.getElementById('button').onclick = function(){
+    // let company = getSelectedOption('companies');
+    let companies = $("#companies").val();
+    console.log("company is", companies)
+    let headers = new Headers()
+    let init = {
+        method : 'GET',
+        headers : headers
+    }
+    let params = {
+        'companies':companies
+    }
+
+    let url = getWebAppBackendUrl('/get_stats')+'/'+JSON.stringify(params)
+    let promise = fetch(url, init) 
+                   .then(function(response){
+                       response.json()
+                               .then(function(data){
+                                // addBarChart(data.chart.data, data.chart.labels);
+                           console.log("data.barChartGroup.company", data.barChartGroup.company)
+                           console.log("data.barChartGroup.labels", data.barChartGroup.labels)
+                                addGroupBarChart(data.barChartGroup.company, 
+                                                 data.barChartGroup.labels);
+                                })
+                   });
+}
+
+
+
+function getSelectedOption(id){
+/* function to get the selected value from the dropdowns */
+    return $("#" + id).val();
+}
+
+
+function setFilterOptions(){
+    /* function to get distinct values of companies and insert them in the dropdowns */
+    let headers = new Headers()
+    let init = {
+        method : 'GET',
+        headers : headers
+    }
+
+    let url = getWebAppBackendUrl('/get_filter_values')
+    let promise = fetch(url, init)
+                   .then(function(response){
+                       response.json()
+                               .then(function(data){
+                                let companiesList = data.companies;
+                                addOptions('companies', companiesList);                                
+                                })
+                   });
+                       
+}
+
+function addRows(id, itemList){
+    /* function to add rows to a table */
+    let itemJson=JSON.parse(itemList)
+    console.log("itemJson", typeof(itemJson))
+    document.getElementById(id).innerHTML = '';
+    i = 0
+    Object.keys(itemJson).forEach(function(key) {
+        // this iterates over rows
+        // console.table('Key : ' + key + ', Value : ' + itemJson[key]);
+        let topics = itemJson[key]["topics"]
+        let averageScores = itemJson[key]["average scores"]
+        let importanceScores = itemJson[key]["importance scores"]
+        // console.log("topics", topics)
+        // console.log("averageScores", averageScores)
+        // console.log("importanceScores", importanceScores)
+        let tbl = document.getElementById(id)        
+        let row = tbl.insertRow(i)
+        row.id = "rowID_" + i;
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        cell1.innerHTML = topics;
+        cell2.innerHTML = averageScores;
+        cell3.innerHTML = importanceScores;
+        i += 1
+})
+    var orderArrayHeader = ["Topics", "Average scores","Importance scores"];
+    var thead = document.createElement('thead');
+    let finishedTable = document.getElementById(id)   
+    finishedTable.appendChild(thead);
+    for(var j=0;j<orderArrayHeader.length;j++){
+        thead.appendChild(document.createElement("th")).
+        appendChild(document.createTextNode(orderArrayHeader[j]));
+    }
+    
+    
+
+    
+    let tblToClick = document.getElementById(id);
+    for (let i = 0; i < tblToClick.rows.length; i++) {
+        for (let j = 0; j < tblToClick.rows[i].cells.length; j++) {
+            tblToClick.rows[i].cells[j].onclick = (function (i, j) {
+                return function () {
+                    // alert('R' + (i + 1) + 'C' + (j + 1));
+                };
+            }(i, j));
+        }
+    }
+}
+
+function addTweetRows(id, itemList){
+    /* function to add rows to a table */
+    console.log("itemList", typeof(itemList));
+    if (typeof(itemList) != 'object') {
+        let itemJson=JSON.parse(itemList);
+    } else {
+        itemJson = itemList
+    }
+    
+    document.getElementById(id).innerHTML = '';
+    let orderArrayHeader = []
+    i = 0
+    Object.keys(itemJson).forEach(function(key) {
+        // this iterates over rows
+        // console.table('Key : ' + key + ', Value : ' + itemJson[key]);
+        let tbl = document.getElementById(id);
+        let row = tbl.insertRow(i);
+        row.id = "rowID_" + i;
+        
+        j=0
+        Object.keys(itemJson[key]).forEach(function(sub_key){
+            cellValue = itemJson[key][sub_key];
+            let cell = row.insertCell(j);
+            cell.innerHTML = cellValue;
+            if (orderArrayHeader.length < Object.keys(itemJson[key]).length){
+                orderArrayHeader.push(sub_key)
+            }
+            j+=1
+        })
+        i += 1
+})
+    
+    let thead = document.createElement('thead');
+    let finishedTable = document.getElementById(id);   
+    finishedTable.appendChild(thead);
+    for(let j=0;j<orderArrayHeader.length;j++){
+        thead.appendChild(document.createElement("th")).
+        appendChild(document.createTextNode(orderArrayHeader[j]));
+    }
+}
+
+function addOptions(id, itemList){
+    /* function to add company choice to the dropdowns */
+    itemList.forEach(function(value, index){
+        let option = document.createElement('option');
+        option.text = value;
+        document.getElementById(id).appendChild(option)
+    })
+}
+
+function addGroupBarChart(data, labels) {
+    document.getElementById('bar-card').innerHTML = '<canvas id="bar-chart-group" chart-click="onClick"></canvas>';
+    i = 1
+    arr = []
+    console.log("labels", labels)
+    data.forEach(function(d){
+        company = Object.keys(d);
+        values = d[company]['data'];
+        dataset_company = {
+            label:company,
+            backgroundColor: default_colors[i],
+            data:values};
+        console.log("dataset_company", dataset_company);
+        arr.push(dataset_company);
+        i+=1;
+    })
+    
+    let data_grouped = {
+        labels: labels,
+        datasets: arr
+              };
+    
+    // this.groupBarChart = 
+    var chart = new Chart(document.getElementById("bar-chart-group"), {
+        type: "bar",
+        data: data_grouped,
+        options: {
+          title: {
+            display: false,
+            text: "Sentiment by category"
+          },
+            onClick: event => {
+                const datasetIndex = chart.getElementAtEvent(event)[0]._datasetIndex;
+                const model = chart.getElementsAtEvent(event)[datasetIndex]._model;
+                onBarClicked(model.datasetLabel, model.label);
+            }
+        }
+    });
+}
---
+++ web_apps/7LEfDjC/backend.py
@@ -0,0 +1,117 @@
+import dataiku
+import pandas as pd
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import time
+from dataiku.core.sql import SQLExecutor2
+import random
+import ast
+
+score_table = "tweepy_analysis_by_companies"
+tweet_id_table = "tweepy_aspect_sentiment_categorised"
+tweets_table = "tweepy_stacked"
+
+
+
+# function to get the dataframe based on the choice from the dropdown
+# columns included: 'group', 'weighted_ave_tb'
+def get_dataset_selection(company):
+    df = dataiku.Dataset(score_table).get_dataframe()
+    # df = df[df.product_id == company][['group', 'weighted_ave_tb']]
+    new = df["product_id"].isin(company)
+    df = df[new][['product_id', 'group', 'weighted_ave_tb']]
+    print("df.columns", df.columns)
+    print("df.product_id.unique()", df.product_id.unique())
+    return df
+
+@app.route('/get_filter_values')
+# function to get unique values for airline companies to show in drop down
+def get_filter_values():
+    df = dataiku.Dataset(score_table).get_dataframe()    
+    companies_list = df['product_id'].unique().tolist()
+    print(companies_list)
+    return json.dumps({'companies': companies_list})
+
+@app.route('/get_stats/<path:params>')
+def get_stats(params):
+    params_dict = json.loads(params)
+    selected_companies = params_dict.get('companies')
+    print("selected_companies is", selected_companies)
+    df = get_dataset_selection(selected_companies)
+    # bar_chart = df.to_dict(orient='records')
+    # bar_chart = {"data":df["weighted_ave_tb"].tolist(), "labels":df["group"].tolist()}
+    # return json.dumps({'chart':bar_chart})
+    global_label = df["group"].unique().tolist()
+    l = []
+    for company in df.product_id.unique():
+        sentiment_score_list = []
+        sub_df = df[df.product_id == company]
+        
+        for g in global_label: # for example, Emirates doesn't have "company" score
+            if g in sub_df.group.unique():
+                value = sub_df[sub_df.group==g].weighted_ave_tb.values[0]
+            else:
+                value = 0
+            sentiment_score_list.append(value)
+        
+        d = {company:{
+            # "labels":sub_df["group"].tolist(), 
+                          "data":sentiment_score_list}}
+        l.append(d)
+    bar_chart_group = {"company":l, "labels":global_label}
+    
+    return json.dumps({'barChartGroup':bar_chart_group})
+    
+@app.route('/get_table/<path:params>')
+def get_table(params):
+    params_dict = json.loads(params)
+    selected_company = params_dict.get('company')[0]
+    selected_category = params_dict.get('category')
+    df = dataiku.Dataset(tweet_id_table).get_dataframe()
+    df_sorted = df[(df.product_id == selected_company) & (df.group == selected_category)].sort_values("tb_importance", ascending = False)
+    df_sorted = df_sorted[["noun_lemmatized", "mean_polarity_textblob", "tb_importance", "review_id"]].rename(columns={"noun_lemmatized":"topics", "mean_polarity_textblob":"average scores", "tb_importance":"importance scores"})
+    top5_pos = df_sorted.head(5).reset_index(drop=True).to_json(orient='index')
+    top5_neg = df_sorted.tail(5).reset_index(drop=True).to_json(orient='index')
+    # top5_pos = top5_pos.to_html(classes=['table', 'table-bordered'], index=False, na_rep='')
+    # top5_neg = top5_neg.to_html(classes=['table', 'table-bordered'], index=False, na_rep='')
+    data = { 'table_pos' : top5_pos, 'table_neg' : top5_neg }
+    json.dumps(data)
+    # return json.dumps({'table_pos':top5_pos, "table_neg":top5_neg})
+    return json.dumps(data)
+    
+    
+    
+    
+@app.route('/get_tweets_table/<path:params>')
+def get_tweets_table(params):
+    params_dict = json.loads(params)
+    company = params_dict.get('company')[0]
+    topic = params_dict.get('topic')
+    print("topic is", topic)
+    tweet_id = params_dict.get('review_id').replace(" ", ", ").replace("/n", "")
+    print("tweet_id:", tweet_id)
+    tweet_id = eval(tweet_id)
+    print("type of ids", type(tweet_id))
+    if len(tweet_id) > 5:
+        id_random_select = random.sample(tweet_id, 5)
+    else:
+        id_random_select = tweet_id
+    
+    id_random_select = map(str, id_random_select)
+    print("randomly selected tweet ids ", id_random_select)
+    df = dataiku.Dataset(tweets_table).get_dataframe()
+    df = df[['timestamp', "company", 'tweet_id', 'text', 'username', 'user_location']]
+    df['tweet_id'] = df.tweet_id.astype(str)
+    df['timestamp'] = df.timestamp.astype(str)
+    print("columns", df.shape)
+    df = df[(df["company"]==company) & (df["tweet_id"].isin(id_random_select))].reset_index(drop=True).to_json(orient='index')
+    print("this is how tweets are returned to JS", df)
+    return df
+    
+    
+    
+    
+    
+    
+    
