3cb6092a7071d3e5f75cbd074028e3b2c605aff9
admin <makoto.miyazaki@dataiku.com>
2020-05-18 11:08:56 +0200

    Saved web app Airline Analysis 0.3 (ANALYSISOFCUSTOMERFRUSTRATIONSINAIRLINEINDUSTRIES.7LEfDjC)

--- web_apps/7LEfDjC/backend.py
+++ web_apps/7LEfDjC/backend.py
@@ -116,7 +116,8 @@ def compare_companies_from_nouns(params):
     params_dict = json.loads(params)
     topic = params_dict.get('topic')
     df = dataiku.Dataset(tweet_id_table).get_dataframe()
-    df_filterred = df[df.noun_lemmatized == topic][['product_id', 'noun_lemmatized', 'mean_polarity_textblob']].to_json(orient='index')
+    df_filterred = df[df.noun_lemmatized == topic][['product_id', 'noun_lemmatized', 'mean_polarity_textblob']].reset_index(drop=True)
+    df_filterred = df_filterred.to_json(orient='index')
     print("df_filterred", df_filterred)
     return df_filterred
     
